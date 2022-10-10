36527a81383ad699fae20b6daf13c37d17d5a5c1
pupil <pupil@ivkhk.ee>
2022-10-10 10:55:33 +0300

    first commit

---
+++ src/jktv21library/App.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv21library;
+
+import java.util.Scanner;
+
+class App {
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        boolean repeat = true;
+        do {
+            System.out.println("функции приложения: ");
+            System.out.println("0 - закрыть приложение.");
+            System.out.println("Выберите номер функции: ");
+            int task = scanner.nextInt();
+            scanner.nextLine();
+            switch (task){
+                case 0:
+                    repeat = false;
+                    break;
+                default:
+                    System.out.println("Выберите номер функции из списка!");
+            }
+        }while(repeat);
+        System.out.println("Chao!");
+    }
+}
